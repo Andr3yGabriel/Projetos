53f8ae83070d6cbc8eedff262ef67f4a60e7144d
Andr3yGabriel <140266061+Andr3yGabriel@users.noreply.github.com>
2023-11-15 18:15:14 -0300

    Add files via upload

--- Projeto Engenharia/main.js
+++ Projeto Engenharia/main.js
@@ -3,16 +3,18 @@ const contas = [];
     function adicionarConta() {
       const nomeConta = document.getElementById('nomeConta').value;
       const valorConta = parseFloat(document.getElementById('valorConta').value);
+      const tipoConta = document.getElementById('tipoConta').value;
 
-      if (!nomeConta || isNaN(valorConta) || valorConta <= 0) {
+      if (!nomeConta || isNaN(valorConta) || valorConta <= 0 || !tipoConta) {
         alert('Por favor, insira um nome de conta válido e um valor positivo.');
         return;
       }
 
-      contas.push({ nome: nomeConta, valor: valorConta });
+      contas.push({ nome: nomeConta, valor: valorConta, tipo: tipoConta });
       atualizarListaContas();
       document.getElementById('nomeConta').value = '';
       document.getElementById('valorConta').value = '';
+      document.getElementById('tipoConta').value = '';
     }
 
     function atualizarListaContas() {
@@ -20,7 +22,7 @@ const contas = [];
       listaContas.innerHTML = '';
       contas.forEach(conta => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${conta.nome}: R$ ${conta.valor}`;
+        listItem.textContent = `${conta.nome}: R$ ${conta.valor} (${conta.tipo})`;
         listaContas.appendChild(listItem);
       });
     }
@@ -37,3 +39,27 @@ const contas = [];
       const porcentagem = ((totalContas * 100) / salario).toFixed(2);
       document.getElementById('porcentagem').textContent = `As contas representam ${porcentagem}% do seu salário.`;
     }
+    const wrapper = document.querySelector('.wrapper');
+    const listaLink = document.querySelector('.listaLink');
+    const analiseLink = document.querySelector('.analiseLink');
+    const btnStart = document.querySelector('.btn-start');
+    const inicioLink = document.querySelectorAll('.inicioLink');
+
+    listaLink.addEventListener('click', ()=> {
+      wrapper.classList.remove('analise');
+      wrapper.classList.add('lista');
+    });
+
+    analiseLink.addEventListener('click', ()=> {
+      wrapper.classList.remove('lista');
+      wrapper.classList.add('analise');
+    });
+
+    function voltarInicio() {
+      wrapper.classList.remove('analise');
+      wrapper.classList.remove('lista');
+    }
+
+    inicioLink.forEach(function(start) {
+      start.addEventListener('click', voltarInicio)
+    })
