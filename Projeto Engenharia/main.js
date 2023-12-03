603a9b39628d668902dfb6907d2e887259a54889
Andr3yGabriel <140266061+Andr3yGabriel@users.noreply.github.com>
2023-12-03 18:30:52 -0300

    Add files via upload

--- Projeto Engenharia/main.js
+++ Projeto Engenharia/main.js
@@ -4,20 +4,29 @@ const contas = [];
       const nomeConta = document.getElementById('nomeConta').value;
       const valorConta = parseFloat(document.getElementById('valorConta').value);
       const tipoConta = document.getElementById('tipoConta').value;
+      const valorSalario = parseFloat(document.getElementById('valorSalario').value);
 
-      if (!nomeConta || isNaN(valorConta) || valorConta <= 0 || !tipoConta) {
+      if (isNaN(valorSalario) ||!nomeConta || isNaN(valorConta) || valorConta <= 0 || !tipoConta) {
         alert('Por favor, insira um nome de conta válido e um valor positivo.');
         return;
       }
 
-      contas.push({ nome: nomeConta, valor: valorConta, tipo: tipoConta });
+      contas.push({ nome: nomeConta, valor: valorConta, tipo: tipoConta, salario: valorSalario});
       atualizarListaContas();
       document.getElementById('nomeConta').value = '';
       document.getElementById('valorConta').value = '';
-      document.getElementById('tipoConta').value = '';
+      document.getElementById('tipoConta').value = 'formaPagamento';
     }
 
     function atualizarListaContas() {
+      const valorSalario = parseFloat(document.getElementById('valorSalario').value);
+      const totalContas = contas.reduce((total, conta) => total + conta.valor, 0);
+      if (totalContas >= valorSalario*0.8 && totalContas <= valorSalario){
+        alert('O seu total de contas está tomando 80% do seu salário, cuidado!');
+      } else if (valorSalario < totalContas) {
+        alert('O total de contas excede o valor do seu salário');
+      };
+      
       const listaContas = document.getElementById('listaContas');
       listaContas.innerHTML = '';
       contas.forEach(conta => {
@@ -25,20 +34,17 @@ const contas = [];
         listItem.textContent = `${conta.nome}: R$ ${conta.valor} (${conta.tipo})`;
         listaContas.appendChild(listItem);
       });
+      alert('Conta adicionada à lista.');
     }
 
     function calcularPorcentagem() {
-      const salario = parseFloat(document.getElementById('salario').value);
-
-      if (isNaN(salario) || salario <= 0) {
-        alert('Por favor, insira um valor de salário válido e positivo.');
-        return;
-      }
-
+      const salario = parseFloat(document.getElementById('valorSalario').value);
       const totalContas = contas.reduce((total, conta) => total + conta.valor, 0);
       const porcentagem = ((totalContas * 100) / salario).toFixed(2);
       document.getElementById('porcentagem').textContent = `As contas representam ${porcentagem}% do seu salário.`;
     }
+
+    
     const wrapper = document.querySelector('.wrapper');
     const listaLink = document.querySelector('.listaLink');
     const analiseLink = document.querySelector('.analiseLink');
